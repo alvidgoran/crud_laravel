087a320d760713dcffdf44805c1a30fe68280ad4
alvidgoran <alexdavidgi16@gmail.com>
2025-05-12 21:11:09 -0500

    probando , supongo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title> esto es lo mejor </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
